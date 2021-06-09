315c1c97c588839742ff8a302e10494259a43908
Steve Calvert <scalvert@linkedin.com>
2021-06-09 08:13:16 -0700

    Adding tests and improving formatting

--- __tests__/index-test.js
+++ __tests__/index-test.js
@@ -1,17 +1,55 @@
 const path = require('path');
+const stripAnsi = require('strip-ansi');
 const { readJsonSync } = require('fs-extra');
 const TodoSummaryFormatter = require('../index');
 
+class MockConsole {
+  buffer = [];
+
+  log(str) {
+    this.buffer.push(str);
+  }
+
+  toString() {
+    return stripAnsi(this.buffer.join('\n'));
+  }
+}
+
 describe('Todo Formatter', () => {
   it('can format output from results', () => {
+    let mockConsole = new MockConsole();
     let todos = readJsonSync(
       path.resolve('./__tests__/__fixtures__/todos.json')
     );
-    debugger;
 
-    let formatter = new TodoSummaryFormatter();
-    let formatted = formatter.print({}, todos);
+    let formatter = new TodoSummaryFormatter({
+      console: mockConsole,
+    });
+    let today = new Date('2021-06-09');
 
-    expect(formatted).toMatchInlineSnapshot(`undefined`);
+    formatter.print({}, todos, today);
+
+    expect(mockConsole.toString()).toMatchInlineSnapshot(`
+"Lint Todos (7 found)
+
+2 are currently past their due date
+┌──────────────────┬───────────────────────────────────────────────────┬────────────┬──────────┐
+│ RuleID           │ filePath                                          │ Due Date   │ Due in   │
+├──────────────────┼───────────────────────────────────────────────────┼────────────┼──────────┤
+│ no-yield-only    │ addon/templates/just-yield.hbs                    │ 05/01/2021 │ -38 days │
+├──────────────────┼───────────────────────────────────────────────────┼────────────┼──────────┤
+│ no-implicit-this │ addon/templates/components/button-toggle.hbs      │ 06/07/2021 │ -1 days  │
+├──────────────────┼───────────────────────────────────────────────────┼────────────┼──────────┤
+│ no-action        │ addon/templates/components/button-toggle.hbs      │ 08/02/2021 │ 54 days  │
+├──────────────────┼───────────────────────────────────────────────────┼────────────┼──────────┤
+│ no-implicit-this │ addon/templates/components/button-toggle.hbs      │ 08/02/2021 │ 54 days  │
+├──────────────────┼───────────────────────────────────────────────────┼────────────┼──────────┤
+│ no-implicit-this │ addon/templates/components/button-toggle.hbs      │ 08/02/2021 │ 54 days  │
+├──────────────────┼───────────────────────────────────────────────────┼────────────┼──────────┤
+│ no-implicit-this │ addon/templates/components/button-toggle.hbs      │ 08/02/2021 │ 54 days  │
+├──────────────────┼───────────────────────────────────────────────────┼────────────┼──────────┤
+│ no-implicit-this │ addon/templates/components/truncate-multiline.hbs │ 08/02/2021 │ 54 days  │
+└──────────────────┴───────────────────────────────────────────────────┴────────────┴──────────┘"
+`);
   });
 });
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const { differenceInDays, format } = require('date-fns');
+const colors = require('colors/safe');
+const Table = require('cli-table3');
 const { readTodosSync } = require('@ember-template-lint/todo-utils');
 
 class TodoSummaryFormatter {
@@ -6,28 +9,54 @@ class TodoSummaryFormatter {
     this.console = options.console || console;
   }
 
-  print(results, todos = this._readTodos(this.options.workingDir)) {
+  print(
+    results,
+    todos = this._readTodos(this.options.workingDir),
+    today = new Date()
+  ) {
     let sorted = todos
       .sort((first, second) => {
-        return second.errorDate - first.errorDate;
+        return first.errorDate - second.errorDate;
       })
       .map((todo) => {
-        let d = new Date(todo.errorDate);
-        let formattedDate = d.toString();
+        let errorDate = new Date(todo.errorDate);
 
         return {
           ruleId: todo.ruleId,
-          formattedDate,
+          filePath: todo.filePath,
+          dueIn: differenceInDays(errorDate, today),
+          date: format(errorDate, 'P'),
+          isExpired: errorDate < today,
         };
       });
 
-    this.console.log('Todo Summary');
+    this.console.log(`Lint Todos (${sorted.length} found)`);
     this.console.log('');
-    this.console.log(`Todos past due date: ${sorted.length}`);
+    this.console.log(
+      `${
+        sorted.filter((todo) => todo.isExpired).length
+      } are currently past their due date`
+    );
+
+    let table = new Table({
+      head: ['RuleID', 'filePath', 'Due Date', 'Due in'],
+      style: {
+        head: ['brightBlue'],
+        border: ['gray'],
+      },
+    });
 
     for (let todo of sorted) {
-      this.console.log(`${todo.ruleId} - ${todo.formattedDate}`);
+      let dueInDays = `${todo.dueIn} days`;
+
+      if (todo.isExpired) {
+        dueInDays = colors.red(dueInDays);
+      }
+
+      table.push([todo.ruleId, todo.filePath, todo.date, dueInDays]);
     }
+
+    this.console.log(table.toString());
   }
 
   _readTodos(baseDir) {
